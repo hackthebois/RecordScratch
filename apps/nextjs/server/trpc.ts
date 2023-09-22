c5df6a6516b29681029bc3cc3a0a1b205e10ed88
Billy Hawkes <billyhawkes02@gmail.com>
2023-09-21 20:11:18 -0400

    Setup initial userId context

--- apps/nextjs/server/trpc.ts
+++ apps/nextjs/server/trpc.ts
@@ -1,6 +1,11 @@
 import { auth } from "@clerk/nextjs";
 import { TRPCError, initTRPC } from "@trpc/server";
-const t = initTRPC.create();
+
+type Context = {
+	userId?: string;
+};
+
+const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
@@ -8,17 +13,15 @@ export const middleware = t.middleware;
 export const mergeRouters = t.mergeRouters;
 
 // User Auth Middleware
-const isAuthed = middleware(({ next }) => {
+const isAuthed = middleware(({ ctx: { userId: initialUserId }, next }) => {
+	if (initialUserId) return next({ ctx: { userId: initialUserId } });
+
 	const { userId } = auth();
 
 	// user not identified
 	if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
 
-	return next({
-		ctx: {
-			userId: userId,
-		},
-	});
+	return next({ ctx: { userId } });
 });
 
 export const protectedProcedure = publicProcedure.use(isAuthed);
