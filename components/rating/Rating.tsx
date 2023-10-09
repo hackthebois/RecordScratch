8f9617c975404af810ba1161d6cb8a96ddc145d9
Billy Hawkes <billyhawkes02@gmail.com>
2023-10-09 17:47:15 -0400

    Added no ratings yet text to artist rating

--- components/rating/Rating.tsx
+++ components/rating/Rating.tsx
@@ -5,9 +5,10 @@ import { Star } from "lucide-react";
 
 type Props = {
 	rating: RatingType | null;
+	emptyText: string;
 };
 
-const Rating = ({ rating }: Props) => {
+const Rating = ({ rating, emptyText }: Props) => {
 	return (
 		<div className="flex items-center gap-2">
 			<Star
@@ -22,7 +23,9 @@ const Rating = ({ rating }: Props) => {
 					</p>
 				)}
 				<p className="text-xs text-muted-foreground">
-					{rating?.totalRatings ?? "Be first to rate!"}
+					{rating?.totalRatings && Number(rating?.totalRatings) !== 0
+						? rating?.totalRatings
+						: emptyText}
 				</p>
 			</div>
 		</div>
