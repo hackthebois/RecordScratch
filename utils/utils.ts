9160a2b2ba7ac08c249054f25edc26061049e575
Billy Hawkes <billyhawkes02@gmail.com>
2023-10-13 07:23:46 -0400

    Fixed build errors

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,16 +1,6 @@
-import { SelectSongRating } from "@/drizzle/db/schema";
-import { SongRatingAverages } from "@/drizzle/db/songFuncs";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
 	return twMerge(clsx(inputs));
-};
-
-// Gets rating from song averages for a specified album
-export const findSongAverage = (
-	songRatings: SongRatingAverages,
-	id: SelectSongRating["songId"]
-) => {
-	return songRatings?.find(({ songId }) => id === songId)?.ratingAverage;
 };
