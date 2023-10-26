c6e8053d458eae74cad45a1f4cf46df73c251753
Billy Hawkes <billyhawkes02@gmail.com>
2023-10-26 16:21:01 -0400

    Changed blue

--- components/rating/RatingButton.tsx
+++ components/rating/RatingButton.tsx
@@ -17,8 +17,8 @@ export const RatingButton = ({
 	return (
 		<Button variant="outline" size="sm" onClick={onClick}>
 			<Star
-				color="#8ecae6"
-				fill={rating ? "#8ecae6" : "none"}
+				color="#219ebc"
+				fill={rating ? "#219ebc" : "none"}
 				size={18}
 				className="mr-2"
 			/>
