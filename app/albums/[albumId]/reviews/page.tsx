7832ae33a2af237babe5ba399d7dc7388d362950
Billy Hawkes <billyhawkes02@gmail.com>
2023-12-04 14:37:04 -0500

    Restyled community reviews

--- app/albums/[albumId]/reviews/page.tsx
+++ app/albums/[albumId]/reviews/page.tsx
@@ -1,12 +1,5 @@
 import { serverTrpc } from "@/app/_trpc/server";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/Card";
 import { Star } from "lucide-react";
 
 const Page = async ({
@@ -21,38 +14,31 @@ const Page = async ({
 	return (
 		<div className="w-full">
 			{reviews.length > 0 ? (
-				reviews.map((review, index) => (
-					<Card className="w-full" key={index}>
-						<CardHeader className="gap-3">
-							{review.title && (
-								<CardTitle>{review.title}</CardTitle>
-							)}
-							<div className="flex gap-1">
-								{Array.from(Array(review.rating)).map(
-									(_, i) => (
-										<Star
-											key={i}
-											size={20}
-											color="#ffb703"
-											fill="#ffb703"
-										/>
-									)
-								)}
-							</div>
-						</CardHeader>
-						{review.description && (
-							<CardContent>{review.description}</CardContent>
-						)}
-						<CardFooter className="gap-3">
-							<Avatar className="h-8 w-8">
-								<AvatarImage src={review.user.imageUrl} />
-								<AvatarFallback />
-							</Avatar>
-							<p>
-								{review.user.firstName} {review.user.lastName}
+				reviews.map(({ user, rating }, index) => (
+					<div
+						className="flex gap-3 rounded-lg border bg-card p-4 text-card-foreground shadow-sm"
+						key={index}
+					>
+						<Avatar className="h-8 w-8">
+							<AvatarImage src={user.imageUrl} />
+							<AvatarFallback />
+						</Avatar>
+						<div className="flex flex-1 items-center justify-between">
+							<p className="font-medium">
+								{user.firstName} {user.lastName}
 							</p>
-						</CardFooter>
-					</Card>
+							<div className="flex gap-1">
+								{Array.from(Array(rating)).map((_, i) => (
+									<Star
+										key={i}
+										size={18}
+										color="#ffb703"
+										fill="#ffb703"
+									/>
+								))}
+							</div>
+						</div>
+					</div>
 				))
 			) : (
 				<p>No reviews yet</p>
