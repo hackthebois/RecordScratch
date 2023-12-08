db23b1691f48ac78dffc29382028366c0a8a84ab
Billy Hawkes <billyhawkes02@gmail.com>
2023-12-07 19:36:21 -0500

    Added onboard name -> handle, setup autofocus on name, merged handle and name, created slide wrapper, setup image types/schema, added skip if not image is selected

--- types/profile.ts
+++ types/profile.ts
@@ -2,12 +2,14 @@ import { profile } from "@/server/db/schema";
 import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
+export const handleRegex = /^[a-zA-Z0-9_]+$/i;
+
 export const ProfileSchema = createSelectSchema(profile, {
 	handle: z
 		.string()
 		.min(1, "Must be at least 1 character")
 		.max(20, "Must be less than 20 characters")
-		.regex(/^[a-zA-Z0-9_]+$/i, "Can only contain letters, numbers, and _"),
+		.regex(handleRegex, "Can only contain letters, numbers, and _"),
 });
 export type Profile = z.infer<typeof ProfileSchema>;
 
