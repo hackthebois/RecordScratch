9db6cfade4ab15db8a3f6ec591f47438bc489d76
Billy Hawkes <billyhawkes02@gmail.com>
2023-12-17 14:37:14 -0500

    centered not found page text

--- app/(app)/not-found.tsx
+++ app/(app)/not-found.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const NotFound = () => {
 	return (
 		<main className="mt-[20vh] flex flex-col items-center justify-center gap-8">
-			<h1>Page Not Found</h1>
-			<p className="text-muted-foreground">
+			<h1 className="text-center">Page Not Found</h1>
+			<p className="text-center text-muted-foreground">
 				Could not find the requested resource
 			</p>
 			<Link href="/" className={buttonVariants()}>
