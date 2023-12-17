cbd8d5b6cb80123ac4aecb267e2f901b486a5850
Billy Hawkes <billyhawkes02@gmail.com>
2023-12-17 14:56:20 -0500

    Fixed accessiblity issues

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -14,7 +14,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 				<nav className="flex w-full max-w-screen-lg items-center justify-between gap-3 p-4 sm:p-6">
 					<div className="flex items-center gap-3">
 						<Link href="/" className="flex items-center gap-3">
-							<Button size="icon" variant="outline">
+							<Button
+								size="icon"
+								variant="outline"
+								aria-label="Home"
+							>
 								<Disc3 size={22} />
 							</Button>
 						</Link>
