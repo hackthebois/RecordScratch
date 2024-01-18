8bae56922cf4e82badf6aeeba5168141a1f6006d
Billy Hawkes <billyhawkes02@gmail.com>
2024-01-18 18:09:25 -0500

    Cleared endpoint

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,15 +1,8 @@
-import { logServerEvent } from "@/server/posthog";
 import { NextResponse } from "next/server";
 
 // Handles GET requests to /api
 export async function GET() {
 	console.log("hello world");
 	// const userRatings = await serverTrpc.user.profile.distribution);
-	await logServerEvent("spotify request", {
-		properties: {
-			url: "/1",
-		},
-		distinctId: "test",
-	});
 	return NextResponse.json({ msg: "Hello world" });
 }
