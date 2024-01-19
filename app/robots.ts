20eeb80e43e7f702a451c4fa561ac48e251a2975
Billy Hawkes <billyhawkes02@gmail.com>
2024-01-19 12:31:12 -0500

    disallow crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from "next";
 
-export default function robots(): MetadataRoute.Robots {
+const robots = async (): Promise<MetadataRoute.Robots> => {
 	return {
 		rules: {
 			userAgent: "*",
-			allow: "/",
+			disallow: "/",
 		},
 		sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
 	};
-}
+};
+
+export default robots;
