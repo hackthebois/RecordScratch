17221c2ac2c93f464ac9c862fd8070fabe8ff3bf
Billy Hawkes <billyhawkes02@gmail.com>
2024-01-21 12:53:48 -0500

    Changed revalidate to 5 to api, switched to canary and added ppr

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer({
 	reactStrictMode: true,
+	experimental: {
+		ppr: true,
+	},
 	images: {
 		unoptimized: true,
 		remotePatterns: [
