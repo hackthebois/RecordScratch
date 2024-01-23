3645376c3e8b0e051dfb52c576513ece1fd9c572
Billy Hawkes <billyhawkes02@gmail.com>
2024-01-23 15:57:34 -0500

    Removed prefetch sign in page, added spotify fetch force cache, updated dep

--- components/SignInWrapper.tsx
+++ components/SignInWrapper.tsx
@@ -7,5 +7,9 @@ import React from "react";
 export const SignInWrapper = ({ children }: { children: React.ReactNode }) => {
 	const pathname = usePathname();
 
-	return <Link href={`/sign-in?redirect_url=${pathname}`}>{children}</Link>;
+	return (
+		<Link href={`/sign-in?redirect_url=${pathname}`} prefetch={false}>
+			{children}
+		</Link>
+	);
 };
