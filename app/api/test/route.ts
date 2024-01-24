c0166e73aa3d4cf058121b56dc2f4bc1a90e8741
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-01-23 19:01:00 -0500

    fixed caching

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 // Handles GET requests to /api
 export async function GET() {
-	const res = await unFollowUser("user_2TMF8gtp24d2Jgn4ua9ETLF0u65");
+	const res = await isUserFollowing("user_2TMF8gtp24d2Jgn4ua9ETLF0u65");
 
 	return NextResponse.json({ res });
 }
