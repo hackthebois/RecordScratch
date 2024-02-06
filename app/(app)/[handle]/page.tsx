243776a5930ac407ab0853cdb8eaac9b208ed082
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-02-06 15:40:45 -0500

    adding category filter for profile ratings

--- app/(app)/[handle]/page.tsx
+++ app/(app)/[handle]/page.tsx
@@ -3,19 +3,21 @@ import {
 	GetInfiniteReviews,
 	InfiniteReviews,
 } from "@/components/resource/InfiniteReviews";
+import { LinkTabs } from "@/components/ui/LinkTabs";
 import { cn } from "@/utils/utils";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
 const Page = async ({
 	params: { handle },
-	searchParams: { rating },
+	searchParams: { rating, category },
 }: {
 	params: {
 		handle: string;
 	};
 	searchParams: {
 		rating: string;
+		category: string;
 	};
 }) => {
 	const profile = await getProfile(handle);
@@ -32,6 +34,10 @@ const Page = async ({
 		return await getRecent({
 			userId: profile.userId,
 			rating: rating ? parseInt(rating) : undefined,
+			category:
+				category === "ALBUM" || category === "SONG"
+					? category
+					: undefined,
 			...input,
 		});
 	};
@@ -76,8 +82,20 @@ const Page = async ({
 					))}
 				</div>
 			</div>
+			<LinkTabs
+				tabs={[
+					{
+						label: "Album",
+						href: `/${profile.handle}?category=ALBUM`,
+					},
+					{
+						label: "Song",
+						href: `/${profile.handle}?category=SONG`,
+					},
+				]}
+			/>
 			<InfiniteReviews
-				key={`${handle}:${rating}`}
+				key={`${profile.handle}:${rating}`}
 				initialReviews={await getReviews({ page: 1, limit: 25 })}
 				getReviews={getReviews}
 				pageLimit={25}
