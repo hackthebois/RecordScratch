87023347440f4536e679033b2d902d03f77f2739
Billy Hawkes <billyhawkes02@gmail.com>
2024-02-08 16:22:57 -0500

    Added handle reserved words

--- types/profile.ts
+++ types/profile.ts
@@ -2,16 +2,47 @@ import { profile } from "@/db/schema";
 import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
-export const handleRegex = /^[a-zA-Z0-9_]+$/i;
+const reserved = [
+	"artists",
+	"artist",
+	"albums",
+	"album",
+	"songs",
+	"song",
+	"about",
+	"terms",
+	"onboard",
+	"api",
+	"list",
+	"playlist",
+	"support",
+	"contact",
+	"friends",
+	"messages",
+	"recommendations",
+	"notifications",
+	"settings",
+	"search",
+	"home",
+];
+
 export const ProfileNameSchema = z
 	.string()
 	.min(1, "Must be at least 1 character")
 	.max(50, "Must be less than 50 characters");
+
+export const handleRegex = /^[a-zA-Z0-9_]+$/i;
 export const ProfileHandleSchema = z
 	.string()
 	.min(1, "Must be at least 1 character")
 	.max(20, "Must be less than 20 characters")
-	.regex(handleRegex, "Must be letters, numbers, or _");
+	.regex(handleRegex, "Must be letters, numbers, or _")
+	.refine((handle) => {
+		if (reserved.includes(handle.toLowerCase())) {
+			return "Handle is reserved";
+		}
+		return true;
+	});
 export const ProfileBioSchema = z
 	.string()
 	.max(200, "Must be less than 200 characters");
