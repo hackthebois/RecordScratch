cd893e92a21367f090c6004621444ec3c64a765e
Billy Hawkes <billyhawkes02@gmail.com>
2024-02-14 16:11:36 -0500

    Fixed base url to deezer in prod

--- app/_api/deezer.ts
+++ app/_api/deezer.ts
@@ -164,6 +164,7 @@ const DeezerSchema = z.object({
 export type Deezer = z.infer<typeof DeezerSchema>;
 
 const getBaseUrl = () => {
+	if (typeof window !== "undefined") return "";
 	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
 	return `http://localhost:${process.env.PORT ?? 3000}`;
 };
