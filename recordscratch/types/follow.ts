753ae95cbd9aa323a83045778ca803ac5c0ab9df
Billy Hawkes <billyhawkes02@gmail.com>
2024-02-15 18:39:20 -0500

    Removed old recordscratch, setup home page loader

--- recordscratch/types/follow.ts
+++
@@ -1,7 +0,0 @@
-import { followers } from "@/recordscratch/server/db/schema";
-import { createSelectSchema } from "drizzle-zod";
-import { z } from "zod";
-
-export const FollowSchema = createSelectSchema(followers);
-
-export type Follow = z.infer<typeof FollowSchema>;
