0dee6870ba3b06a3b0101b0a3fec20ed09dce746
Billy Hawkes <billyhawkes02@gmail.com>
2024-02-16 11:41:44 -0500

    Fixed song page formatting

--- app/routes/_app/albums/$albumId/songs/$songId/index.tsx
+++ app/routes/_app/albums/$albumId/songs/$songId/index.tsx
@@ -45,36 +45,38 @@ function Song() {
 	};
 
 	return (
-		<div className="flex flex-col gap-6 sm:flex-row sm:items-start">
-			{song.album.cover_big && (
-				<img
-					width={250}
-					height={250}
-					alt={`${song.album.title} cover`}
-					src={song.album.cover_big}
-					className="w-[250px] self-center rounded-xl"
-				/>
-			)}
-			<div className="flex flex-col items-center gap-4 sm:items-start">
-				<p className="text-sm tracking-widest text-muted-foreground">SONG</p>
-				<h1 className="text-center sm:text-left">{song.title}</h1>
-				<div className="flex flex-wrap justify-center gap-3 sm:justify-start">
-					<Tag variant="outline">{album.release_date}</Tag>
-					{song.explicit_lyrics && <Tag variant="outline">Explicit</Tag>}
-					<Tag variant="outline">{formatMs(song.duration * 1000)}</Tag>
+		<div className="flex flex-col gap-6">
+			<div className="flex flex-col gap-6 sm:flex-row sm:items-start">
+				{song.album.cover_big && (
+					<img
+						width={250}
+						height={250}
+						alt={`${song.album.title} cover`}
+						src={song.album.cover_big}
+						className="w-[250px] self-center rounded-xl"
+					/>
+				)}
+				<div className="flex flex-col items-center gap-4 sm:items-start">
+					<p className="text-sm tracking-widest text-muted-foreground">SONG</p>
+					<h1 className="text-center sm:text-left">{song.title}</h1>
+					<div className="flex flex-wrap justify-center gap-3 sm:justify-start">
+						<Tag variant="outline">{album.release_date}</Tag>
+						{song.explicit_lyrics && <Tag variant="outline">Explicit</Tag>}
+						<Tag variant="outline">{formatMs(song.duration * 1000)}</Tag>
+					</div>
+					<Link
+						to="/albums/$albumId"
+						params={{
+							albumId: String(album.id),
+						}}
+						className={buttonVariants({
+							size: "sm",
+							variant: "secondary",
+						})}
+					>
+						Go to album
+					</Link>
 				</div>
-				<Link
-					to="/albums/$albumId"
-					params={{
-						albumId: String(album.id),
-					}}
-					className={buttonVariants({
-						size: "sm",
-						variant: "secondary",
-					})}
-				>
-					Go to album
-				</Link>
 			</div>
 			<Tabs defaultValue="reviews">
 				<TabsList>
