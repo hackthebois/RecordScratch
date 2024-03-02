86e03a1d71a570edb181b45ac4485a920cb76063
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-03-02 17:48:27 -0500

    adds tabs to lists page

--- app/routes/_app/lists/$listId/index.tsx
+++ app/routes/_app/lists/$listId/index.tsx
@@ -1,21 +1,39 @@
 import { PendingComponent } from "@/components/router/Pending";
-import { ErrorComponent, Link, createFileRoute } from "@tanstack/react-router";
+import {
+	ErrorComponent,
+	Link,
+	createFileRoute,
+	useNavigate,
+} from "@tanstack/react-router";
 import { api } from "@/trpc/react";
 import { Head } from "@/components/Head";
 import { NotFound } from "@/components/ui/NotFound";
 import Metadata from "@/components/Metadata";
 import { UserAvatar } from "@/components/UserAvatar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
+import { z } from "zod";
 
 export const Route = createFileRoute("/_app/lists/$listId/")({
 	component: List,
 	pendingComponent: PendingComponent,
 	errorComponent: ErrorComponent,
+	validateSearch: (search) => {
+		return z
+			.object({
+				tab: z.enum(["list"]).optional(),
+			})
+			.parse(search);
+	},
 });
 
 const undefinedImage =
 	"https://e-cdns-images.dzcdn.net/images/artist//500x500-000000-80-0-0.jpg";
 
 function List() {
+	const navigate = useNavigate({
+		from: Route.fullPath,
+	});
+	const { tab = "list" } = Route.useSearch();
 	const { listId } = Route.useParams();
 
 	const { data: listData, isLoading } = api.lists.getList.useQuery({
@@ -29,10 +47,7 @@ function List() {
 
 	return (
 		<div className="flex flex-col gap-6">
-			<Head
-				title={listData.name}
-				description={listData.description ?? ""}
-			/>
+			<Head title={listData.name} description={undefined} />
 			<Metadata
 				title={listData.name}
 				cover={undefinedImage}
@@ -51,6 +66,25 @@ function List() {
 				</Link>
 				<h5>{listData.description}</h5>
 			</Metadata>
+			<Tabs value={tab}>
+				<TabsList>
+					<TabsTrigger
+						value="list"
+						onClick={() =>
+							navigate({
+								search: {
+									tab: undefined,
+								},
+							})
+						}
+					>
+						List
+					</TabsTrigger>
+				</TabsList>
+				<TabsContent value="list">
+					{/* <SongTable songs={album.tracks?.data ?? []} /> */}
+				</TabsContent>
+			</Tabs>
 		</div>
 	);
 }
