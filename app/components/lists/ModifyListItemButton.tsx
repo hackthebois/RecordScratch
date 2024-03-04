28509e98e6029b89253d1aab12e96999105a3591
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-03-04 00:40:13 -0500

    added parent id to list_resources and changed add/delete button for lists

--- app/components/lists/ModifyListItemButton.tsx
+++ app/components/lists/ModifyListItemButton.tsx
@@ -1,59 +1,61 @@
 import { api } from "@/trpc/react";
 import { Button } from "../ui/Button";
 
-export const ModifyListItemButton = (inputs: {
-	parentId: string;
+export const DeleteListItemButton = (inputs: {
 	resourceId: string;
 	listId: string;
-	type: "ADD" | "DELETE";
 }) => {
 	const utils = api.useUtils();
 
-	if (inputs.type === "ADD") {
-		const addResource = api.lists.resources.createListResource.useMutation({
-			onSettled: () => {
-				utils.lists.resources.getListResources.invalidate({
-					listId: inputs.listId,
-				});
-			},
-		}).mutate;
+	const deleteResource = api.lists.resources.deleteListResource.useMutation({
+		onSettled: () => {
+			utils.lists.resources.getListResources.invalidate({
+				listId: inputs.listId,
+			});
+		},
+	}).mutate;
+	return (
+		<div>
+			<Button
+				className="bg-red-200"
+				onClick={() =>
+					deleteResource({
+						...inputs,
+					})
+				}
+			>
+				Delete
+			</Button>
+		</div>
+	);
+};
 
-		return (
-			<div>
-				<Button
-					onClick={() =>
-						addResource({
-							...inputs,
-						})
-					}
-				>
-					ADD
-				</Button>
-			</div>
-		);
-	}
-	if (inputs.type === "DELETE") {
-		const deleteResource =
-			api.lists.resources.deleteListResource.useMutation({
-				onSettled: () => {
-					utils.lists.resources.getListResources.invalidate({
-						listId: inputs.listId,
-					});
-				},
-			}).mutate;
-		return (
-			<div>
-				<Button
-					className="bg-red-200"
-					onClick={() =>
-						deleteResource({
-							...inputs,
-						})
-					}
-				>
-					Delete
-				</Button>
-			</div>
-		);
-	}
+export const AddListItemButton = (inputs: {
+	parentId?: string;
+	resourceId: string;
+	listId: string;
+}) => {
+	const utils = api.useUtils();
+
+	const addResource = api.lists.resources.createListResource.useMutation({
+		onSettled: () => {
+			utils.lists.resources.getListResources.invalidate({
+				listId: inputs.listId,
+			});
+		},
+	}).mutate;
+
+	return (
+		<div>
+			<Button
+				onClick={() =>
+					addResource({
+						...inputs,
+					})
+				}
+			>
+				ADD
+			</Button>
+		</div>
+	);
 };
