2d4633365a5bc631d92281aa6b25a96da60a4dd6
Billy Hawkes <billyhawkes02@gmail.com>
2024-03-05 18:16:47 -0500

    Added review time and profile handle

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -1,30 +1,10 @@
+import dayjs from "dayjs";
+import RelativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(RelativeTime);
+
 export const timeAgo = (date: Date): string => {
-	const now = new Date();
-	const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-
-	let interval = Math.floor(seconds / 31536000);
-
-	if (interval > 1) {
-		return `${interval} years ago`;
-	}
-	interval = Math.floor(seconds / 2592000);
-	if (interval > 1) {
-		return `${interval} months ago`;
-	}
-	interval = Math.floor(seconds / 86400);
-	if (interval > 1) {
-		return `${interval} days ago`;
-	}
-	interval = Math.floor(seconds / 3600);
-	if (interval > 1) {
-		return `${interval} hours ago`;
-	}
-	interval = Math.floor(seconds / 60);
-	if (interval > 1) {
-		return `${interval} minutes ago`;
-	}
-
-	return `${Math.floor(seconds)} seconds ago`;
+	return dayjs(date).fromNow();
 };
 
 export const formatMs = (milliseconds: number): string => {
