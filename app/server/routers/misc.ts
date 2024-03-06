93e8185182d4ea86223b8c3f1a53ac797c9726c0
Billy Hawkes <billyhawkes02@gmail.com>
2024-03-06 08:16:17 -0500

    Added new albums of the day, moved to backend, added fallback to yesterday if none exists

---
+++ app/server/routers/misc.ts
@@ -0,0 +1,191 @@
+import { publicProcedure, router } from "../trpc";
+
+const albums = [
+	{
+		albumId: "44730061",
+		date: new Date(2024, 1, 23),
+	},
+	{
+		albumId: "542677642",
+		date: new Date(2024, 1, 24),
+	},
+	{
+		albumId: "117320",
+		date: new Date(2024, 1, 25),
+	},
+	{
+		albumId: "6982611",
+		date: new Date(2024, 1, 26),
+	},
+	{
+		albumId: "423868847",
+		date: new Date(2024, 1, 27),
+	},
+	{
+		albumId: "60322892",
+		date: new Date(2024, 1, 28),
+	},
+	{
+		albumId: "1440807",
+		date: new Date(2024, 1, 29),
+	},
+	{
+		albumId: "93038342",
+		date: new Date(2024, 2, 1),
+	},
+	{
+		albumId: "107638",
+		date: new Date(2024, 2, 2),
+	},
+	{
+		albumId: "384314",
+		date: new Date(2024, 2, 3),
+	},
+	{
+		albumId: "423868847",
+		date: new Date(2024, 2, 4),
+	},
+	{
+		albumId: "549643892",
+		date: new Date(2024, 2, 5),
+	},
+	{
+		albumId: "548477832",
+		date: new Date(2024, 2, 6),
+	},
+	{
+		albumId: "405219857",
+		date: new Date(2024, 2, 7),
+	},
+	{
+		albumId: "",
+		date: new Date(2024, 2, 8),
+	},
+	{
+		albumId: "451265235",
+		date: new Date(2024, 2, 9),
+	},
+	{
+		albumId: "104660202",
+		date: new Date(2024, 2, 10),
+	},
+	{
+		albumId: "257803",
+		date: new Date(2024, 2, 11),
+	},
+	{
+		albumId: "44730061",
+		date: new Date(2024, 2, 12),
+	},
+	{
+		albumId: "539734212",
+		date: new Date(2024, 2, 13),
+	},
+	{
+		albumId: "1440807",
+		date: new Date(2024, 2, 14),
+	},
+	{
+		albumId: "65371772",
+		date: new Date(2024, 2, 15),
+	},
+	{
+		albumId: "11205422",
+		date: new Date(2024, 2, 16),
+	},
+	{
+		albumId: "6575789",
+		date: new Date(2024, 2, 17),
+	},
+	{
+		albumId: "60322892",
+		date: new Date(2024, 2, 18),
+	},
+	{
+		albumId: "14880659",
+		date: new Date(2024, 2, 19),
+	},
+	{
+		albumId: "504180521",
+		date: new Date(2024, 2, 20),
+	},
+	{
+		albumId: "455130",
+		date: new Date(2024, 2, 21),
+	},
+	{
+		albumId: "9236757",
+		date: new Date(2024, 2, 22),
+	},
+	{
+		albumId: "1452138",
+		date: new Date(2024, 2, 23),
+	},
+	{
+		albumId: "1233211",
+		date: new Date(2024, 2, 24),
+	},
+	{
+		albumId: "413452427",
+		date: new Date(2024, 2, 25),
+	},
+	{
+		albumId: "11591214",
+		date: new Date(2024, 2, 26),
+	},
+	{
+		albumId: "249353",
+		date: new Date(2024, 2, 27),
+	},
+	{
+		albumId: "7291105",
+		date: new Date(2024, 2, 28),
+	},
+	{
+		albumId: "86994002",
+		date: new Date(2024, 2, 29),
+	},
+	{
+		albumId: "43265861",
+		date: new Date(2024, 2, 30),
+	},
+	{
+		albumId: "47131362",
+		date: new Date(2024, 2, 31),
+	},
+	{
+		albumId: "10709540",
+		date: new Date(2024, 3, 1),
+	},
+];
+
+const isCurrentDay = (date: Date) => {
+	const currentDate = new Date();
+	// Check if the current date matches the day to show
+	return (
+		currentDate.getDate() === date.getDate() &&
+		currentDate.getMonth() === date.getMonth() &&
+		currentDate.getFullYear() === date.getFullYear()
+	);
+};
+
+export const miscRouter = router({
+	albumOfTheDay: publicProcedure.query(() => {
+		const albumOfTheDay = albums.find((album) => {
+			if (isCurrentDay(album.date) && album.albumId !== "") {
+				return true;
+			}
+		});
+		if (albumOfTheDay) return albumOfTheDay;
+
+		// If there is no album of the day, return yesterday's album
+		const yesterday = new Date();
+		yesterday.setDate(yesterday.getDate() - 1);
+		const yesterdaysAlbum = albums.find((album) => {
+			if (album.date.getDate() === yesterday.getDate()) {
+				return true;
+			}
+		});
+		return yesterdaysAlbum ? yesterdaysAlbum : albums[0];
+	}),
+});
