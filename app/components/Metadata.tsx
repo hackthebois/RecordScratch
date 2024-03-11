1ea3dab4bc64e9060853c26a012e9881ed8a93f5
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-03-11 17:07:13 -0400

    fixed several bugs

--- app/components/Metadata.tsx
+++ app/components/Metadata.tsx
@@ -6,31 +6,24 @@ const Metadata = ({
 	type,
 	tags,
 	children,
-	ownImage,
-	Image,
 }: {
 	title: string;
 	cover?: string;
 	type: string;
 	tags: (string | undefined)[];
 	children: React.ReactNode;
-	ownImage?: boolean;
-	Image?: React.ReactNode;
 }) => {
 	return (
 		<>
 			<div className="flex flex-col gap-6 sm:flex-row sm:items-start">
-				{!ownImage ? (
-					<img
-						width={200}
-						height={200}
-						alt={`${title} cover`}
-						src={cover}
-						className="h-[200px] w-[200px] self-center rounded-xl sm:self-start"
-					/>
-				) : (
-					<div className=" self-center">{Image}</div>
-				)}
+				<img
+					width={200}
+					height={200}
+					alt={`${title} cover`}
+					src={cover}
+					className="h-[200px] w-[200px] self-center rounded-xl sm:self-start"
+				/>
+
 				<div className="flex flex-col items-center gap-4 sm:items-start">
 					<p className="-mb-2 text-sm tracking-widest text-muted-foreground">
 						{type.toUpperCase()}
