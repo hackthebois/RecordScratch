a799ba1dd72338e9f438e276391396566b4d5e8d
Billy Hawkes <billyhawkes02@gmail.com>
2024-03-12 17:06:55 -0400

    Fixed not found page button

--- app/components/ui/NotFound.tsx
+++ app/components/ui/NotFound.tsx
@@ -1,14 +1,14 @@
+import { Link } from "@tanstack/react-router";
+import { buttonVariants } from "./Button";
+
 export const NotFound = () => {
 	return (
 		<div className="flex flex-col items-center">
 			<h1 className="text-[80px] font-extrabold">404</h1>
 			<p className="mb-6 text-2xl font-medium">Page Not Found</p>
-			<a
-				href="/"
-				className="rounded-md bg-gray-100 px-4 py-2 font-medium text-black transition-all duration-200 ease-in-out hover:bg-indigo-600"
-			>
+			<Link to="/" className={buttonVariants()}>
 				Go Home
-			</a>
+			</Link>
 		</div>
 	);
 };
