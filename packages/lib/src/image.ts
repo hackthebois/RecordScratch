bef43651d546a612965288e81596a07c6f8e31e2
Billy Hawkes <billyhawkes02@gmail.com>
2024-04-04 21:13:55 -0400

    Fixed profile images

--- packages/lib/src/image.ts
+++
@@ -1,5 +0,0 @@
-import type { Profile } from "@recordscratch/types";
-
-export const getImageUrl = (profile: Profile) => {
-	return `${process.env.VITE_R2_PUBLIC_URL}/profile-images/${profile.userId}?updatedAt=${new Date(profile.updatedAt).getTime()}`;
-};
--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./date";
 export * from "./deezer";
 export * from "./hooks";
-export * from "./image";
 export * from "./recents";
 export * from "./utils";
