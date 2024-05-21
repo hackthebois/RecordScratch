bc3e07044cead38e9b037bd77ececbe97258f0f1
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-05-21 00:41:05 -0400

    cloudflare

--- apps/web/app/routes/__root.tsx
+++ apps/web/app/routes/__root.tsx
@@ -55,7 +55,7 @@ const PostHogIdentify = () => {
 function Root() {
 	return (
 		<PostHogProvider
-			apiKey={env.VITE_POSTHOG_KEY}
+			apiKey={process.env.VITE_POSTHOG_KEY}
 			options={{
 				api_host: process.env.CF_PAGES_URL + "/ingest",
 			}}
