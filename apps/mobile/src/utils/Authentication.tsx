0fbb10107da11be2f9185d3ac0d8d7bdb437f8b8
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-07-04 00:16:27 -0400

    Fixing Mobile Auth

--- apps/mobile/src/utils/Authentication.tsx
+++ apps/mobile/src/utils/Authentication.tsx
@@ -15,7 +15,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const useAuth = () => {
 	const context = useContext(AuthContext);
 	if (context === undefined) {
-		throw new Error("useAuth must be used within AuthProvider");
+		throw new Error("useAuth must be used within an AuthProvider");
 	}
 	return context;
 };
