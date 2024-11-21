c61d8745c7bf6d9ec2e4780188fba2f029bf67dd
Billy Hawkes <billyhawkes02@gmail.com>
2024-11-21 17:07:02 -0500

    version

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,6 +1,6 @@
 import type { ConfigContext, ExpoConfig } from "@expo/config";
 
-const version = "0.0.3";
+const version = "0.0.4";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
