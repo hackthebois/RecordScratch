19b533d0b3ef8acf8d93766f3861c32318ba9d19
Billy Hawkes <billyhawkes02@gmail.com>
2025-01-31 17:36:15 -0500

    fix: following/followers

--- apps/mobile/src/app/(tabs)/(feed,home,search,notifications,profile)/[handle]/followers.tsx
+++ apps/mobile/src/app/(tabs)/(feed,home,search,notifications,profile)/[handle]/followers.tsx
@@ -1,5 +1,6 @@
 import NotFoundScreen from "@/app/+not-found";
 import { ProfileItem } from "@/components/Item/ProfileItem";
+import { WebWrapper } from "@/components/WebWrapper";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Text } from "@/components/ui/text";
 import { api } from "@/lib/api";
@@ -9,61 +10,70 @@ import { FlashList } from "@shopify/flash-list";
 import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { useState } from "react";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 
 const Followers = () => {
-	const { handle, type } = useLocalSearchParams<{
-		handle: string;
-		type: string;
-	}>();
-	const [tab, setTab] = useState(type ?? "followers");
-	const profile = useAuth((s) => s.profile);
+  const { handle, type } = useLocalSearchParams<{
+    handle: string;
+    type: string;
+  }>();
+  const [tab, setTab] = useState(type ?? "followers");
+  const profile = useAuth((s) => s.profile);
 
-	const [userProfile] = api.profiles.get.useSuspenseQuery(handle);
+  const [userProfile] = api.profiles.get.useSuspenseQuery(handle);
 
-	if (!userProfile) return <NotFoundScreen />;
+  if (!userProfile) return <NotFoundScreen />;
 
-	const { data: followProfiles, refetch } = api.profiles.followProfiles.useQuery({
-		profileId: userProfile.userId,
-		type: tab === "followers" ? "followers" : "following",
-	});
+  const { data: followProfiles, refetch } =
+    api.profiles.followProfiles.useQuery({
+      profileId: userProfile.userId,
+      type: tab === "followers" ? "followers" : "following",
+    });
 
-	const { refetchByUser, isRefetchingByUser } = useRefreshByUser(refetch);
+  const { refetchByUser, isRefetchingByUser } = useRefreshByUser(refetch);
 
-	return (
-		<>
-			<Stack.Screen
-				options={{
-					title: tab === "followers" ? "Followers" : "Following",
-					headerBackVisible: true,
-				}}
-			/>
-			<Tabs value={tab} onValueChange={setTab} className="flex-1">
-				<View className="w-full px-4">
-					<TabsList className="flex-row">
-						<TabsTrigger value="followers" className="flex-1">
-							<Text>Followers</Text>
-						</TabsTrigger>
-						<TabsTrigger value="following" className="flex-1">
-							<Text>Following</Text>
-						</TabsTrigger>
-					</TabsList>
-				</View>
-				<FlashList
-					data={followProfiles?.flatMap((item) => item.profile)}
-					renderItem={({ item }) => (
-						<ProfileItem profile={item} isUser={profile!.userId === item.userId} />
-					)}
-					estimatedItemSize={60}
-					ItemSeparatorComponent={() => <View className="h-3" />}
-					className="px-4"
-					contentContainerClassName="py-4"
-					refreshing={isRefetchingByUser}
-					onRefresh={refetchByUser}
-				/>
-			</Tabs>
-		</>
-	);
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: tab === "followers" ? "Followers" : "Following",
+          headerBackVisible: true,
+          headerShown: Platform.OS !== "web",
+        }}
+      />
+      <Tabs value={tab} onValueChange={setTab} className="flex-1 sm:mt-4">
+        <WebWrapper>
+          <View className="w-full px-4">
+            <TabsList className="flex-row">
+              <TabsTrigger value="followers" className="flex-1">
+                <Text>Followers</Text>
+              </TabsTrigger>
+              <TabsTrigger value="following" className="flex-1">
+                <Text>Following</Text>
+              </TabsTrigger>
+            </TabsList>
+          </View>
+        </WebWrapper>
+        <FlashList
+          data={followProfiles?.flatMap((item) => item.profile)}
+          renderItem={({ item }) => (
+            <WebWrapper>
+              <ProfileItem
+                profile={item}
+                isUser={profile!.userId === item.userId}
+              />
+            </WebWrapper>
+          )}
+          estimatedItemSize={60}
+          ItemSeparatorComponent={() => <View className="h-3" />}
+          className="px-4"
+          contentContainerClassName="py-4"
+          refreshing={isRefetchingByUser}
+          onRefresh={refetchByUser}
+        />
+      </Tabs>
+    </>
+  );
 };
 
 export default Followers;
