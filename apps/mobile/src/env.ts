2d5bf133c5365ce1fd6c5e0909285317596a14b3
Billy Hawkes <billyhawkes02@gmail.com>
2025-02-02 14:35:29 -0500

    fix: env for prod

--- apps/mobile/src/env.ts
+++ apps/mobile/src/env.ts
@@ -1,16 +1,17 @@
 import * as Updates from "expo-updates";
 import { Platform } from "react-native";
 
-console.log("PROCESS.ENV", process.env);
-
 let env = {
   ENV: "development",
   R2_PUBLIC_URL: "https://cdn.recordscratch.app",
-  SCHEME: Platform.OS === "web" ? "http://localhost:8081/" : "recordscratch://",
+  SCHEME:
+    Platform.OS === "web"
+      ? "https://recordscratch-refactor.pages.dev/"
+      : "recordscratch://",
   SITE_URL:
     Platform.OS === "android"
       ? "https://humane-cockatoo-instantly.ngrok-free.app"
-      : "http://localhost:3000",
+      : "https://server.recordscratchapp.workers.dev",
   DEBUG: true,
 };
 
